54c2b32e25d26e0906064b1eb27ff4095ed0a84f
Alanzink1 <alanrod37831@gmail.com>
2024-08-21 15:27:35 -0300

    feat: add logic for github client using it for the useRepos hook

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = () => {
         <>
         <h1 className='title'>Welcome to RepoWars</h1>
         <h2 className="headline gradient text">A developer-driven guessing game</h2>
+        
         {repos.length == 0 ? (
           <p className="loading">Loading game...</p>
         ): (<button type="submit" className="button gradient" onClick={handleStartClick}>Start</button>)}
