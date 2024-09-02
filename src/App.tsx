be5ca35b9c1ee18154229bf8270c4ba96e5520bd
Alanzink1 <alanrod37831@gmail.com>
2024-09-01 21:43:53 -0300

    feat: add RepoCard Component with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react"
 import "./app.css"
 import { useRepos } from "./hooks"
+import { Game } from "./components"
 
 const App = () => {
   const [showGame, setShowGame] = useState<boolean>(false)
@@ -16,18 +17,16 @@ const App = () => {
         <>
         <h1 className='title'>Welcome to RepoWars</h1>
         <h2 className="headline gradient text">A developer-driven guessing game</h2>
-        
         {repos.length == 0 ? (
           <p className="loading">Loading game...</p>
         ): (<button type="submit" className="button gradient" onClick={handleStartClick}>Start</button>)}
         </>
       )}
-      {showGame && (
-        // TODO: Create Game Component
-        <>
-          <h1>Game</h1>
-        </>
-      )}
+      {showGame && <Game 
+      repoState={[repos, setRepos]}
+      originalList={allRepos}
+      setShowGame={setShowGame}
+      />}
     </main>
   )
 }
---
+++ src/components/Game.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import { FaExchangeAlt } from 'react-icons/fa'
+import './game.css'
+import RepoCard from './RepoCard'
+
+interface GameProps {
+    repoState: [Repo[], React.Dispatch<React.SetStateAction<Repo[]>>]
+    originalList: Repo[],
+    setShowGame: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Game: React.FC<GameProps> = ({
+    repoState,
+    originalList,
+    setShowGame}) => {
+    const [over, setOver] = useState<boolean>(false)
+    const [repo1, setRepo1] = useState<Repo | null>(null)
+    const [repo2, setRepo2] = useState<Repo | null>(null)
+    // TODO:  Use actual random repos
+    useEffect(()=>{
+        setRepo1(originalList[0])
+        setRepo2(originalList[1])
+    }, [])
+
+    // Handlers:
+        const handleChoice = (r: Repo) =>{}
+
+    return <div className='game'>
+    {over ? <div className='score'></div> : repo1 && repo2 &&
+        (<div className='repos'>
+            <h1 className='title'>Choose the Repo with Most Stars</h1>
+            <div className='container'>
+                <RepoCard content={repo1} handler={handleChoice}/>
+
+                <div className='dashboard'>
+                    <FaExchangeAlt className='icon-versus'/>
+                </div>
+
+                <RepoCard content={repo2} handler={handleChoice}/>
+            </div>
+        </div>)}
+
+        <div className='buttons'>
+            <button type='submit' className='button gradient alternate' onClick={()=>setShowGame(false)}>
+                {over ? "Play Again" : "Back"}
+            </button>
+                {!over && (
+                    <button type='submit' className='button gradient' 
+                    // TODO: change this button to refresh the current repositories
+                    onClick={()=>setShowGame(false)}>
+                    Skip
+                    </button>
+                )}
+        </div>
+
+    </div>
+}
+
+export default Game
