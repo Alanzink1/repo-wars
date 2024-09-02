ad1a898f76f7c9018afede7ceb0ffd87d098dc06
Alanzink1 <alanrod37831@gmail.com>
2024-09-02 00:14:18 -0300

    feat: deploy configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/repo-wars",
   resolve: {alias: {"node-fetch": "axios"}},
   define: {
     global: {},
