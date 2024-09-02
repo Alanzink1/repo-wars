97b4350867ef460fbd3931dd748e26950e1ee6d2
Alanzink1 <alanrod37831@gmail.com>
2024-09-01 23:44:44 -0300

    fix: height description removed

--- src/components/repoCard.tsx
+++ src/components/repoCard.tsx
@@ -6,16 +6,16 @@ import { VscCommentDiscussion, VscIssues, VscProject } from 'react-icons/vsc'
 import { truncate } from '../utils'
 import './repo-card.css'
 
-interface RepoCardProps{
+interface RepoCardProps {
     content: Repo
     handler: (r: Repo) => void
 }
 
 const RepoCard: React.FC<RepoCardProps> = ({ content, handler }) => {
-    return <div className="repo" onClick={()=>handler(content)}>
+    return <div className="repo" onClick={() => handler(content)}>
         <div className="header">
             <h1 className="name">{content.name}</h1>
-            <h2 className="fullname">{content.full_name}</h2>    
+            <h2 className="fullname">{content.full_name}</h2>
         </div>
         <div className="content">
             <p className="description">{truncate(content.description, 72)}</p>
@@ -30,30 +30,31 @@ const RepoCard: React.FC<RepoCardProps> = ({ content, handler }) => {
                         <p>{content.forks_count}</p>
                         <FaCodeBranch />
                     </li>
-                    <div className="booleans"></div>
-                    <li className="item">
-                        <TbPageBreak className={`item-${content.has_pages}`} />
-                    </li>
-                    <li className="item">
-                        <VscIssues className={`item-${content.has_issues}`} />
-                    </li>
-                    <li className="item">
-                        <VscCommentDiscussion className={`item-${content.has_discussions}`} />
-                    </li>
-                    <li className="item">
-                        <BsBook className={`item-${content.has_wiki}`} />
-                    </li>
-                    <li className="item">
-                        <FaDownload className={`item-${content.has_downloads}`} />
-                    </li>
-                    <li className="item">
-                        <VscProject className={`item-${content.has_projects}`} />
-                    </li>
+                    <div className="booleans">
+                        <li className="item">
+                            <TbPageBreak className={`item-${content.has_pages}`} />
+                        </li>
+                        <li className="item">
+                            <VscIssues className={`item-${content.has_issues}`} />
+                        </li>
+                        <li className="item">
+                            <VscCommentDiscussion className={`item-${content.has_discussions}`} />
+                        </li>
+                        <li className="item">
+                            <BsBook className={`item-${content.has_wiki}`} />
+                        </li>
+                        <li className="item">
+                            <FaDownload className={`item-${content.has_downloads}`} />
+                        </li>
+                        <li className="item">
+                            <VscProject className={`item-${content.has_projects}`} />
+                        </li>
+                    </div>
                     <div className="topics">
-                        {content.topics.map(topic=>(<li
-                        className='topic' key={topic}>
+                        {content.topics.map((topic) => (<li
+                            className='topic' key={topic}>
                             <p>{topic}</p>
-                            </li>))}
+                        </li>))}
                     </div>
                 </ul>
             </div>
